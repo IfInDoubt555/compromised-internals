6594ea432e0bf8fba706bb81e0ac2773157f0e1d
Chris Brown <99719676+IfInDoubt555@users.noreply.github.com>
2025-04-19 08:59:37 -0400

    Added mt-6 to checkout.success block

--- resources/views/shop/checkout/success.blade.php
+++ resources/views/shop/checkout/success.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-w-2xl mx-auto mt-0 bg-white shadow rounded p-8 text-center">
+<div class="max-w-2xl mx-auto mt-6 bg-white shadow rounded p-8 text-center">
     <h1 class="text-3xl font-bold mb-4 text-green-600">Payment Successful 🎉</h1>
     <p class="text-gray-700 mb-6">Thank you for your purchase! Your order is being processed.</p>
 
