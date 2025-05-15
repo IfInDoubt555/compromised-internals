4f04e6087cc60e3857fa0bf703cc299fcf48aa6e
Chris Brown <99719676+IfInDoubt555@users.noreply.github.com>
2025-05-15 16:58:15 -0400

    Updated Contact page
    -Updated Note up top
    -cleaned up value= fields

--- resources/views/footer/contact.blade.php
+++ resources/views/footer/contact.blade.php
@@ -6,21 +6,21 @@
 
     <form action="{{ route('contact.submit') }}" method="POST" class="space-y-6 bg-white dark:bg-gray-500 p-6 rounded-xl shadow">
         <div class="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mt-4 mb-6 rounded shadow-md text-sm">
-            <strong>Heads up!</strong> This site is in early testing. If you spot any bugs or have suggestions, please use this form to let me know!
+            <strong>Heads up!</strong> This site is in early testing. If you spot any bugs or have suggestions, please use this form to let me know! You don't have to use real information for the NAME and EMAIL fiels below. test@test.com for the email field is fine.
         </div>
 
         @csrf
 
         <div>
             <label for="name" class="block font-semibold mb-1">Name</label>
-            <input type="text" id="name" name="name" value="{{ $errors->any() ? old('name') : '' }}"
+            <input type="text" id="name" name="name" value="{{ old('name') }}"
                 class="w-full border border-gray-300 dark:border-gray-700 rounded px-4 py-2 dark:bg-gray-300" required>
             @error('name') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
         </div>
 
         <div>
             <label for="email" class="block font-semibold mb-1">Email</label>
-            <input type="email" id="email" name="email" value="{{ $errors->any() ? old('name') : '' }}"
+            <input type="email" id="email" name="email" value="{{ old('email') }}"
                 class="w-full border border-gray-300 dark:border-gray-600 rounded px-4 py-2 dark:bg-gray-300" required>
             @error('email') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
         </div>
@@ -29,7 +29,7 @@
             <label for="message" class="block font-semibold mb-1">Message</label>
             <textarea id="message" name="message" rows="6"
                 class="w-full border border-gray-300 dark:border-gray-600 rounded px-4 py-2 dark:bg-gray-300"
-                required>{{ $errors->any() ? old('message') : '' }}</textarea>
+                required>{{ old('message') }}</textarea>
             @error('message') <p class="text-red-500 text-sm mt-1">{{ $message }}</p> @enderror
         </div>
 
