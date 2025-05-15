3c46ccc2476c1221ff02bdde422f01b962fd83e2
Chris Brown <99719676+IfInDoubt555@users.noreply.github.com>
2025-05-15 06:19:06 -0400

    added history.js to vite config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/css/fade.css',
                 'resources/js/app.js',
+                'resources/js/history.js'
             ],
             refresh: true,
         }),
