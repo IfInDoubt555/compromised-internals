1e9d99a036ac637fa2b3f41a7fac90a2cc13f4f6
Chris Brown <99719676+IfInDoubt555@users.noreply.github.com>
2025-05-15 08:42:14 -0400

    trying to load default white shirt through product-image.blade

--- resources/views/components/product-image.blade.php
+++ resources/views/components/product-image.blade.php
@@ -1,10 +1,10 @@
 <div class="flex justify-center">
     <img
         id="productImage"
-        src="{{ asset($image) }}"
+        src="{{ asset($colors['white']) }}" {{-- Load white as default --}}
         alt="Product Image"
         class="rounded-lg w-full max-w-md h-auto"
-        @foreach($colors as $color=> $path)
+        @foreach ($colors as $color=> $path)
     data-{{ $color }}="{{ asset($path) }}"
     @endforeach
     />
