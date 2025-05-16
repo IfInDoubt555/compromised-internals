ef47f39235d2002e021bd03aaf5628324316320a
Chris Brown <99719676+IfInDoubt555@users.noreply.github.com>
2025-05-16 15:04:45 -0400

    Moved Favicon to Public
    Changes Image link in app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;700&family=Inter&display=swap" rel="stylesheet" />
-    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <!-- Vite Build -->
     @vite(['resources/css/app.css', 'resources/css/fade.css', 'resources/js/app.js'])
