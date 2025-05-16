7808e276bd02e3fd5ab9a412a14ab0e01e6af72d
Chris Brown <99719676+IfInDoubt555@users.noreply.github.com>
2025-05-16 07:50:22 -0400

    updated No-Cache

--- app/Http/Middleware/NoCache.php
+++ app/Http/Middleware/NoCache.php
@@ -4,11 +4,10 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class NoCache
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
         return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate');
