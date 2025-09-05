40a43803146b0bb7c10c663ff25c26a51be16fcf
IfInDoubt555 <99719676+IfInDoubt555@users.noreply.github.com>
2025-09-05 09:40:06 -0400

    Added use App\Models\OrderItem; to Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\OrderItem;
 
 /**
  * @use \Illuminate\Database\Eloquent\Factories\HasFactory<\Database\Factories\OrderFactory>
